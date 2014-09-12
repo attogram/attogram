9175de4134a4f0c380f3c3b8e4f954e38f4db3cd
David B <attogram@users.noreply.github.com>
2014-09-12 22:39:13 +0200

    restruct error setup

--- plugins/error.php
+++ plugins/error.php
@@ -3,19 +3,24 @@
 
 class plugin_error { 
 
+  var $attogram;
+  
   function is_active() { return true; } 
 
   function __construct($attogram='') { $this->attogram = $attogram; } 
 
   function hook($hook) { 
     if( $hook == 'POST-ADMIN' ) { print '<br />plugin_error: ' . ($this->is_active() ? 'ACTIVE' : 'DISABLED'); return; }  
-    if( $hook == 'ERROR_QUERY' ) { 
-      $ei = @$this->attogram->db->errorInfo();
-      print '<pre>ERROR_QUERY: ' . @$this->attogram->db->errorCode() . ' ' . $ei[2] . '</pre>';
-      return;
-    } 
     if( !preg_match('/^ERROR/', $hook) ) { return; } 
-    print '<pre>Attogram ERROR: ' . $hook . '</pre>';
-  } 
+    switch( $hook ) {
+      case 'ERROR-QUERY':
+        $ei = @$this->attogram->db->errorInfo();
+        print '<pre>ERROR-QUERY: ' . @$this->attogram->db->errorCode() . ' ' . $ei[2] . '</pre>';
+        break;
+      default:
+        print '<pre>' . $hook . '</pre>';
+        break;
+    }
+  }
 
 } // END of class plugin_error
