e31c262396bf1d24ef288a1942e2887ac3fc4e0e
David B <attogram@users.noreply.github.com>
2014-09-12 13:07:41 +0200

    fixy for the POST-FOOTER

--- templates/footer.php
+++ templates/footer.php
@@ -1,11 +1,10 @@
 <?php 
 // Attogram - template - footer
-$this->hook('PRE-FOOTER');
 
+$this->hook('PRE-FOOTER');
 ?><div class="footer">
 <a href="/attogram/">Attogram PHP Framework</a> @ <?php print gmdate('Y-m-d H:i:s'); ?> UTC<br />
-<?php if( $this->is_admin() ) { ?><a href="/attogram/admin/">admin</a>:<?php } ?> 
-user: <?php print $_SERVER['REMOTE_ADDR']; ?> 
+<?php if( $this->is_admin() ) { ?><a href="/attogram/admin/">admin</a>:<?php } ?>
+user: <?php print $_SERVER['REMOTE_ADDR']; ?>
+<?php $this->hook('POST-FOOTER'); ?>
 </div></body></html>
-<?php
-$this->hook('POST_FOOTER');
