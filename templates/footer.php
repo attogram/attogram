35fb2ad1b42cc9fcb25990b6264f3b13d508f7c1
David B <attogram@users.noreply.github.com>
2014-10-06 22:46:35 +0200

    use this->path

--- templates/footer.php
+++ templates/footer.php
@@ -3,8 +3,8 @@
 
 $this->hook('PRE-FOOTER');
 ?><div class="footer">
-<a href="/attogram/">Attogram PHP Framework</a> @ <?php print gmdate('Y-m-d H:i:s'); ?> UTC<br />
-<?php if( $this->is_admin() ) { ?><a href="/attogram/admin/">admin</a>:<?php } ?>
+<a href="<?php print $this->path; ?>/">Attogram PHP Framework</a> @ <?php print gmdate('Y-m-d H:i:s'); ?> UTC<br />
+<?php if( $this->is_admin() ) { ?><a href="<?php print $this->path; ?>/admin/">admin</a>:<?php } ?>
 user: <?php print $_SERVER['REMOTE_ADDR']; ?>
 <?php $this->hook('POST-FOOTER'); ?>
 </div></body></html>
