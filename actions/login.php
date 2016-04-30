ca1ed9ff4de6b9dc8a85c1979dea6be252ca6580
David B <barberi.office@gmail.com>
2016-04-30 16:44:02 +0200

    rm pading

--- actions/login.php
+++ actions/login.php
@@ -5,12 +5,10 @@ include('templates/header.php');
 <div class="body">
 
 <form action="" method="POST">
-  <p style="padding:10;">
-    <input type="hidden" name="login" value="login">
-    <p>Username: <input type="text" name="u" size="25"></p>
-    <p>Password: <input type="password" name="p" size="25"></p>
-    <p><input type="submit" value="                Login                "></p>
-  </p>
+<input type="hidden" name="login" value="login">
+<p>Username: <input type="text" name="u" size="25"></p>
+<p>Password: <input type="password" name="p" size="25"></p>
+<p><input type="submit" value="                Login                "></p>
 </form>
 <?php
 	if( isset($_POST['login']) ) { login(); }
