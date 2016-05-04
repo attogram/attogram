4cd3749d7968e376b1f351c5537bdaf970081ff9
David B <barberi.office@gmail.com>
2016-05-04 22:42:02 +0200

    always check for db error

--- plugins/error.php
+++ plugins/error.php
@@ -10,24 +10,10 @@ class plugin_error {
   function __construct($attogram='') { $this->attogram = $attogram; } 
 
   function hook($hook) {
-
     if( !preg_match('/^ERROR/', $hook) ) { return; } // only do ERROR hooks
-
-    switch( $hook ) {
-
-      case 'ERROR-QUERY':
-        $ei = @$this->attogram->db->errorInfo();
-        print '<pre>ERROR-QUERY: ' . @$this->attogram->error 
-        . ' SQLSTATE:' . @$ei[0] 
-        . ' code:' . @$ei[1] 
-        . ' message:' . @$ei[2] . '</pre>';
-        break;
-
-      default:
-        print '<pre>' . $hook . ': ' . @$this->attogram->error . '</pre>';
-        break;
-
-    }
+    print '<pre>' . $hook . ': ' . @$this->attogram->error . '</pre>';
+    $ei = @$this->attogram->db->errorInfo();
+    if( isset($ei[0]) && $ei[0] != '0000') { print '<pre>' . $hook . ': SQLSTATE:' . @$ei[0] . ' code:' . @$ei[1] . ' message:' . @$ei[2] . '</pre>'; }
   }
 
 } // END of class plugin_error
