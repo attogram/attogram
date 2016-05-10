47df4c55d67cd18caf8a93b7d442f43543861c93
David B <barberi.office@gmail.com>
2016-05-10 19:35:18 +0200

    mv util functions to main index.php

--- index.php
+++ index.php
@@ -264,6 +264,7 @@ class attogram {
 } // END of class attogram
 
 
+
 //////////////////////////////////////////////////////////////////////
 class sqlite_database {
 
@@ -405,3 +406,42 @@ class sqlite_database {
   }
   
 } // END of class sqlite_database
+
+
+
+// Global Utility Functions
+
+////////////////////////////////////////////////////////////////////
+function is_readable_dir( $dir=FALSE ) {
+  if( is_dir($dir) && is_readable($dir) ) {
+    return TRUE;
+  }
+  return FALSE;
+}
+
+//////////////////////////////////////////////////////////////////////
+function is_readable_php_file( $file=FALSE ) {
+  if( is_file($file) && is_readable($file) && preg_match('/\.php$/',$file) ) {
+    return TRUE;
+  }
+  return FALSE;
+}
+
+//////////////////////////////////////////////
+function to_list($x) {
+  if( is_array($x) ) {
+    $r = '';
+    foreach($x as $v) {
+      if( !is_object($v) && !is_array($v) ) {
+        $r .= $v . ', ';
+      } else {
+        $r .= to_list($v);
+      }
+    }
+    return trim($r,', ');
+  }
+  if( is_object($x) ) {
+    return print_r($x,1) . '<br />';
+  }
+  return $x;
+}
