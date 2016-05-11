345d92875add16ae0f603a6fec8274b2c0732381
David B <barberi.office@gmail.com>
2016-05-11 23:56:10 +0200

    error checks and genericize

--- actions/readme.php
+++ actions/readme.php
@@ -2,18 +2,19 @@
 // Attogram - action - readme
 
 $title = 'Attogram - Readme';
-
 include($this->templates_dir . '/header.php');
 print '<div class="body">';
 
-$pd = new Parsedown();
-
-$rf = file_get_contents('readme.md');
-
-$r = $pd->text($rf);
-
-print $r;
+$file = 'readme.md';
+if( is_file($file) && is_readable($file) ) {
+  if( class_exists('Parsedown') ) {
+    print Parsedown::instance()->text( file_get_contents($file) );
+  } else {
+    print 'Error: can not find parser';
+  }
+} else {
+  print 'Error: can not read ' . $file;
+}
 
 print '</div>';
-
 include($this->templates_dir . '/footer.php');
