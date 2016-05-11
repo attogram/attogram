fb066dcb28a2bc8c5e1de4cf50c173e851b5ecac
David B <barberi.office@gmail.com>
2016-05-11 23:44:09 +0200

    parsedown intro mod

--- functions/Parsedown.php
+++ functions/Parsedown.php
@@ -1,17 +1,11 @@
 <?php
+# Attogram distribution of Parsedown:
 
-#
-#
 # Parsedown
 # http://parsedown.org
 #
 # (c) Emanuil Rusev
 # http://erusev.com
-#
-# For the full license information, view the LICENSE file that was distributed
-# with this source code.  Or view below:
-#
-#
 /*
 The MIT License (MIT)
 
