e46f31f310a3a535705ca545043e0c2d5bf1dafc
David B <barberi.office@gmail.com>
2016-05-13 14:28:35 +0200

    clean

--- actions/home.php
+++ actions/home.php
@@ -1,7 +1,8 @@
 <?php
 // Attogram - action - home
+
 include($this->templates_dir . '/header.php');
-$this->hook('PRE-HOME');
+
 ?>
 <style>
 ul, ol {
@@ -18,51 +19,44 @@ ul, ol {
 
 <div class="container">
 
-  <div class="row"> 
+  <div class="row">
 
     <div class="col-sm-3">
       <h3>Pages:</h3>
       <ul>
       <?php
-      foreach( $this->actions as $action ){
-        print '<li><a href="./' . $action . '/">' . $action . '</a></li>';
-      }
+        foreach( $this->actions as $action ){
+          print '<li><a href="./' . $action . '/">' . $action . '</a></li>';
+        }
       ?>
-    </ul>
-  </div>
-    
-    <div class="col-sm-3">
-<?php
-    if( $this->is_admin() ) {
-  $this->get_admin_actions();
-  print '<h3>Admin:</h3><ul>';
-  foreach( $this->admin_actions as $action ){
-    print '<li><a href="./' . $action . '/">' . $action . '</a></li>';
-  }
-  print '</ul>';
-}
-?>
+      </ul>
     </div>
+
+    <div class="col-sm-3">
+      <?php
+        if( $this->is_admin() ) {
+          $this->get_admin_actions();
+          print '<h3>Admin:</h3><ul>';
+          foreach( $this->admin_actions as $action ){
+            print '<li><a href="./' . $action . '/">' . $action . '</a></li>';
+          }
+          print '</ul>';
+        }
+      ?>
+    </div>
+
     <div class="col-sm-6">
       <p>The Attogram Framework gives developers a jumpstart for creating new web sites.</p>
       <p>Attogram uses PHP with Apache2.  It includes URL routing, an IP-protected backend, a user system,
-      an integrated SQLite database with web administration, jQuery and Bootstrap.  
+      an integrated SQLite database with web administration, jQuery and Bootstrap.
       After that, Attogram tries to stay out of your way while you build what you want!</p>
       <p><a href="https://github.com/attogram/attogram">Download Attogram @ GitHub</a>
-    </div> 
+    </div>
   </div>
-</div>
 
-
-
-
-
-
-
-
-
+  </div>
 
 </div>
+
 <?php
-$this->hook('POST-HOME');
 include($this->templates_dir . '/footer.php');
