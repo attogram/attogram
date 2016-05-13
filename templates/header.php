b75c76c4718b74ec834db2e8ccf51063f6852db2
David B <barberi.office@gmail.com>
2016-05-13 14:41:01 +0200

    bootstrap fonts, move to own directory

--- templates/header.php
+++ templates/header.php
@@ -12,10 +12,10 @@ if( !isset($title) || !$title ) { $title = 'Attogram Framework'; }
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="<?php print $this->path; ?>/web/css.css">
-<link rel="stylesheet" href="<?php print $this->path; ?>/web/bootstrap.min.css">
+<link rel="stylesheet" href="<?php print $this->path; ?>/web/bootstrap/css/bootstrap.min.css">
 <title><?php print $title; ?></title>
 <script src="<?php print $this->path; ?>/web/jquery.min.js"></script>
-<script src="<?php print $this->path; ?>/web/bootstrap.min.js"></script>
+<script src="<?php print $this->path; ?>/web/bootstrap/js/bootstrap.min.js"></script>
 </head>
 <body>
 <?php
