363e3380634aa1ece46a2121e1451c814b0eb248
David B <barberi.office@gmail.com>
2016-05-14 23:49:23 +0200

    fix form action

--- actions/login.php
+++ actions/login.php
@@ -13,7 +13,7 @@ $this->page_header('Attogram - Login');
 ?>
 <div class="container">
 <?php if( $message ) { print "<pre>$message</pre>"; } ?>
-<form action="" method="POST">
+<form action="." method="POST">
 <input type="hidden" name="login" value="login">
 <p>Username: <input type="text" name="u" size="25"></p>
 <p>Password: <input type="password" name="p" size="25"></p>
