0b5e7b0cc54937fa970f91030dc403b9de832c23
David B <barberi.office@gmail.com>
2016-05-14 21:42:10 +0200

    Version 0.3.0

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 /* *******************************************************************
 
 Attogram Framework
-Version 0.2.9
+Version 0.3.0
 
 Copyright (c) 2016 Attogram Developers
 https://github.com/attogram/attogram/
@@ -31,7 +31,7 @@ class attogram {
    * @return void
    */
   function __construct() {
-    $this->version = '0.2.9';
+    $this->version = '0.3.0';
     $this->load_config('config.php');
     $this->sessioning();
     $this->skip_files = array('.','..','.htaccess');
