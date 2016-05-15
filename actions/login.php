9900698cdcc2bcefa18da57ded66ee6130bbd968
David B <barberi.office@gmail.com>
2016-05-15 20:24:32 +0200

    bootstrap styles

--- actions/login.php
+++ actions/login.php
@@ -6,20 +6,25 @@ if( isset($_POST['login']) ) { // attempt to login, buffer errors to show later
     header('Location: ' . $this->path . '/');
     exit;
   }
-  $message = 'Login failed';
+  $message = '<p class="alert alert-warning">Login failed</p>';
 }
 
 $this->page_header('Attogram - Login');
 ?>
 <div class="container">
-<?php if( $message ) { print "<pre>$message</pre>"; } ?>
-<form action="." method="POST">
-<input type="hidden" name="login" value="login">
-<p>Username: <input type="text" name="u" size="25"></p>
-<p>Password: <input type="password" name="p" size="25"></p>
-<p><input type="submit" value="                Login                "></p>
-</form>
-
+  <?php if( $message ) { print $message; } ?>
+  <form action="." method="POST">
+    <div class="form-group">
+      <input type="hidden" name="login" value="login">
+    </div>
+    <div class="form-group">
+      Username: <input class="form-control" type="text" name="u">
+    </div>
+    <div class="form-group">
+      Password: <input class="form-control" type="password" name="p">
+    </div>
+    <button type="submit" class="btn btn-info" style="width:50%;"> Login </button>
+  </form>
 </div>
 <?php
 $this->page_footer();
