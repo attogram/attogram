80993c89ab85a9b828c822573b39b2f9d1b4dc48
David B <barberi.office@gmail.com>
2016-05-15 15:18:39 +0200

    actions to navbar, new homepage

--- actions/home.php
+++ actions/home.php
@@ -8,54 +8,16 @@ $this->page_header('Attogram Framework');
 ?>
 
 <div class="jumbotron text-center">
-
   <h1>Attogram Framework</h1>
-
   <p>giving developers a jumpstart to quickly create web sites</p>
-
   <p><a href="https://github.com/attogram/attogram"><span class="glyphicon glyphicon-search" aria-hidden="true"></span> View Attogram on GitHub</a></p>
-
   <p><a href="https://github.com/attogram/attogram/archive/master.zip"><span class="glyphicon glyphicon-download-alt" aria-hidden="true"></span> Download Attogram Now!</a></p>
-
 </div>
 
 <div class="container">
-
-  <div class="row">
-
-    <div class="col-sm-3">
-      <h3>Pages:</h3>
-      <ul>
-      <?php
-        foreach( array_keys($this->get_actions()) as $action ){
-          if( $action == 'home' ) { continue; }
-          print '<li><a href="' . $action . '/">' . $action . '</a></li>';
-        }
-      ?>
-      </ul>
-    </div>
-
-    <div class="col-sm-3">
-      <?php
-        if( $this->is_admin() ) {
-          $this->get_admin_actions();
-          print '<h3>Admin:</h3><ul>';
-          foreach( array_keys($this->get_admin_actions()) as $action ){
-            print '<li><a href="' . $action . '/">' . $action . '</a></li>';
-          }
-          print '</ul>';
-        }
-      ?>
-    </div>
-
-    <div class="col-sm-6">
-      <p>Attogram runs on <b>PHP</b> with <b>Apache</b>.</p>
-      <p>It provides <b>URL routing</b>, an IP-protected <b>backend</b>, a <b>user system</b>,
-      a <b>SQLite</b> database with web admin, a <b>Markdown</b> parser, <b>jQuery</b> and <b>Bootstrap</b>.</p>
-      <p>After that, Attogram tries to stay out of your way while you <b>do your thing</b>!</p>
-    </div>
-  </div>
-
+  <p>Attogram runs on <b>PHP</b> with <b>Apache</b>.</p>
+  <p>It provides <b>URL routing</b>, an IP-protected <b>backend</b>, a <b>user system</b>, a <b>SQLite</b> database with web admin, a <b>Markdown</b> parser, <b>jQuery</b> and <b>Bootstrap</b>.</p>
+  <p>After that, Attogram tries to stay out of your way while you <b>do your thing</b>!</p>
 </div>
 
 <?php
--- templates/navbar.php
+++ templates/navbar.php
@@ -19,25 +19,10 @@
 
     <div id="navbar" class="navbar-collapse collapse">
 
-      <ul class="nav navbar-nav navbar-right">
-      <?php if( $this->is_admin() ) {  ?>
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Admin <span class="caret"></span></a>
-          <ul class="dropdown-menu">
-          <?php
-          foreach( array_keys($this->get_admin_actions()) as $a ) {
-            print '<li><a href="' . $this->path . '/' . $a . '/">' . $a . '</a></li>';
-          } ?>
-          </ul>
-        </li>
-      <?php } ?>
-
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Pages <span class="caret"></span></a>
-          <ul class="dropdown-menu">
-          <?php
-          print '<li><a href="' . $this->path . '/"><span class="glyphicon glyphicon-home" aria-hidden="true"></span> home</a></li>';
-          foreach( array_keys($this->get_actions()) as $a ) {
+      <ul class="nav navbar-nav">
+        <?php
+        
+        foreach( array_keys($this->get_actions()) as $a ) {
             if( $a == 'home' ) { continue; }
             if( $a == 'login' && $this->is_logged_in() ) { continue; }
             if( $a == 'user' && !$this->is_logged_in() ) { continue; }
@@ -49,11 +34,26 @@
           }
           if( $this->is_logged_in() ) {
             print '<li><a href="?logoff"><span class="glyphicon glyphicon-remove-circle" aria-hidden="true"></span> logoff</a></li>';
+          } 
+       ?>
+      </ul>
+
+      <?php if( $this->is_admin() ) {  ?>
+            <ul class="nav navbar-nav navbar-right">
+
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Admin <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+          <?php
+          foreach( array_keys($this->get_admin_actions()) as $a ) {
+            print '<li><a href="' . $this->path . '/' . $a . '/">' . $a . '</a></li>';
           } ?>
           </ul>
         </li>
+              </ul>
 
-      </ul>
+      <?php } ?>
+
     </div><!--/.nav-collapse -->
   </div><!--/.container-fluid -->
 </nav>
