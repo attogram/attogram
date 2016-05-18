ef48ec3caa5e2a499e2d0055a4077cbc96449e13
David B <barberi.office@gmail.com>
2016-05-18 23:01:14 +0200

    info_file(), info_dir()

--- admin/info.php
+++ admin/info.php
@@ -5,35 +5,45 @@ namespace Attogram;
 
 $this->page_header('Attogram - Admin - Info');
 
+function info_file($file) {
+  if( is_file($file) && is_readable($file) ) { $gn = 'ok'; $gt = 'success'; } else { $gn = 'remove'; $gt = 'danger'; }
+  return '<span class="glyphicon glyphicon-' . $gn . ' text-' . $gt . '" aria-hidden="true"></span> ' . $file;  
+}
+
+function info_dir($dir) {
+  if( is_readable_dir($dir) ) { $gn = 'ok'; $gt = 'success'; } else { $gn = 'remove'; $gt = 'danger'; }
+  return '<span class="glyphicon glyphicon-' . $gn . ' text-' . $gt . '" aria-hidden="true"></span> ' . $dir;
+}
+
 $info = array();
 $info['ATTOGRAM_VERSION'] = ATTOGRAM_VERSION;
 $info['site_name'] = $this->site_name;
 $info['site_url'] = '<a href="' . $this->get_site_url() . '">' . $this->get_site_url() . '</a>';
 $info['path'] = ( $this->path ? $this->path : '<code>empty</code>' );
 $info['uri'] = '<li>' . to_list($this->uri,'<li>');
-$info['action'] = $this->action;
+$info['action'] = info_file($this->action);
 
 $autoloader = 'vendor/autoload.php';
-$info['autoloader'] = ( is_readable_file($autoloader, '.php') ? $autoloader : '<code>null</code>' );
+$info['autoloader'] = info_file($autoloader);
 $info['log'] = ( is_object($this->log) ? get_class($this->log) : '<code>?</code>' );
 
-$info['actions_dir'] = $this->actions_dir;
-$info['default_action'] = $this->default_action;
+$info['actions_dir'] = info_dir($this->actions_dir);
+$info['default_action'] = info_file($this->default_action);
 $info['actions'] = list_actions($this->actions);
 
-$info['admin_dir'] = $this->admin_dir;
+$info['admin_dir'] = info_dir($this->admin_dir);
 $info['admin_actions'] = list_actions($this->admin_actions);
 $info['admins'] = '<li>' . to_list($this->admins, '<li>');
 
 $info['fof'] = $this->fof;
-$info['templates_dir '] = $this->templates_dir ;
-$info['functions_dir'] = $this->functions_dir;
-$info['skip_files'] = '<li>' . to_list($this->skip_files, '<li>');
+$info['templates_dir '] = info_dir($this->templates_dir);
+$info['functions_dir'] = info_dir($this->functions_dir);
 
-$info['db_name'] = $this->db_name;
-$info['tables_dir'] = $this->tables_dir;
+$info['db_name'] = info_file($this->db_name);
+$info['tables_dir'] = info_dir($this->tables_dir);
 $info['database_size'] = (file_exists($this->db_name) ? filesize($this->db_name) : '<code>null</code>') . ' bytes';
 $info['sqlite_database'] = get_class($this->sqlite_database);
+$info['skip_files'] = '<li>' . to_list($this->skip_files, '<li>');
 
 $info['attogram_id'] = isset($_SESSION['attogram_id']) ? htmlentities($_SESSION['attogram_id']) : '<code>null</code>';
 $info['attogram_username'] = isset($_SESSION['attogram_username']) ? htmlentities($_SESSION['attogram_username']) : '<code>null</code>';
