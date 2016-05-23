4fd408f01bdaf584ca53111f7ccef7dc74d47cf6
David B <barberi.office@gmail.com>
2016-05-23 22:04:06 +0200

    use tabler()

--- admin/users.php
+++ admin/users.php
@@ -1,34 +1,26 @@
 <?php
-// Attogram - action - admin - users
+namespace Attogram;
 
 $this->page_header('Attogram - Admin - Users');
 
-$users = $this->sqlite_database->query('SELECT * FROM user ORDER BY id');
-
-print '<div class="container"><p><strong>' . count($users) . '</strong> <a href="">Users</a>';
-print ' &nbsp; - &nbsp; <a target="_db" href="../db-admin/?table=user&action=row_create">Create New User</a></p>';
-print '<table class="table table-bordered"><thead><tr>
-<th>ID</th>
-<th>edit</th>
-<th>delete</th>
-<th>username</th>
-<th>password</th>
-<th>email</th>
-<th>level</th>
-<th>last_login</th>
-<th>last_host</th>
-</tr></thead><tbody>';
-foreach($users as $u) {
-  print '<tr><td>' . $u['id'] . '</td>';
-  print '<td><a target="_db" href="../db-admin/?table=user&action=row_editordelete&pk=[' . $u['id'] . ']&type=edit">edit</a></td>';
-  print '<td><a target="_db" href="../db-admin/?table=user&action=row_editordelete&pk=[' . $u['id'] . ']&type=delete">delete</a></td>';
-  print '<td>' . htmlentities($u['username']) . '</td>';
-  print '<td>' . htmlentities($u['password']) . '</td>';
-  print '<td>' . htmlentities($u['email']) . '</td>';
-  print '<td>' . htmlentities($u['level']) . '</td>';
-  print '<td>' . htmlentities($u['last_login']) . '</td>';
-  print '<td>' . htmlentities($u['last_host']) . '</td></tr>';
-}
-print '</tbody></table></div>';
+tabler(
+  $attogram = $this,
+  $table = 'user',
+  $name_singular = 'user',
+  $name_plural = 'users',
+  $public_link = FALSE,
+  $col = array(
+    array('class'=>'col-md-1', 'title'=>'<code>ID</code>', 'key'=>'id'),
+    array('class'=>'col-md-5', 'title'=>'username', 'key'=>'username'),
+    array('class'=>'col-md-1', 'title'=>'password', 'key'=>'password'),
+    array('class'=>'col-md-1', 'title'=>'email', 'key'=>'email'),
+    array('class'=>'col-md-1', 'title'=>'level', 'key'=>'level'),
+    array('class'=>'col-md-1', 'title'=>'last_login', 'key'=>'last_login'),
+    array('class'=>'col-md-1', 'title'=>'last_host', 'key'=>'last_host'),
+  ),
+  $sql = 'SELECT * FROM user ORDER BY id',
+  $admin_link = '../users/',
+  $show_edit = TRUE
+);
 
 $this->page_footer();
