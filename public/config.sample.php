ef0d9aeec97db4b17d38b815adee3f2b44769b05
David B <barberi.office@gmail.com>
2016-05-28 22:23:51 +0200

    example config

--- public/config.sample.php
+++ public/config.sample.php
@@ -1,23 +1,30 @@
 <?php
-// Attogram config
+// Attogram Framework - Main Configuration File
 
 namespace Attogram;
+
 $config = array();
 
 // Uncomment to change default values...
 
-//$config['attogram_directory'] = '../';  // The Attogram installation directory, with trailing slash
+// The Attogram installation directory, with trailing slash
+// $config['attogram_directory'] = '../';
 
-//$config['debug'] = FALSE; // Debug/logging - TRUE or FALSE
+// Debug/logging - TRUE or FALSE
+// $config['debug'] = FALSE;
 
-//$config['site_name'] = 'Attogram Framework <small>v' . ATTOGRAM_VERSION . '</small>'; 
+// The Site Name
+// $config['site_name'] = 'Attogram Framework <small>v' . ATTOGRAM_VERSION . '</small>';
 
-//$config['admins'] = array( '127.0.0.1', '::1', );
+// List of Administrator IPs
+// $config['admins'] = array( '127.0.0.1', '::1', );
 
+// Depth Settings
 // By default, all pages are allowed 2 levels (depth) in the URLs
 // To change this setting per page, set the depth here:
-//$config['depth']['insert-action-name-here'] = 2;
+// $config['depth']['insert-action-name-here'] = 3;
 
+// End Slash Settings
 // By default, a slash / is forced onto the end of all URLs, if not already present.
 // To allow slash-less ending URLs, list the action names here:
-//$config['force_slash_exceptions'][] = 'insert_action_name_here';
+// $config['force_slash_exceptions'][] = 'insert_action_name_here';
