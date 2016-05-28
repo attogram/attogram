482a332f540b03cefd9d75e64035eb7f0d8c33ab
David B <barberi.office@gmail.com>
2016-05-28 09:47:58 +0200

    config example depth, force_slash_exceptions

--- public/config.sample.php
+++ public/config.sample.php
@@ -14,7 +14,10 @@ $config = array();
 
 //$config['admins'] = array( '127.0.0.1', '::1', );
 
-//$config['depth'] = array( 
-//  '*' => 2, // the default depth
-//  ''  => 1, // depth for the home page
-//);
+// By default, all pages are allowed 2 levels (depth) in the URLs
+// To change this setting per page, set the depth here:
+//$config['depth']['insert-action-name-here'] = 2;
+
+// By default, a slash / is forced onto the end of all URLs, if not already present.
+// To allow slash-less ending URLs, list the action names here:
+//$config['force_slash_exceptions'][] = 'insert_action_name_here';
