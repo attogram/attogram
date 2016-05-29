49ea7a18fddd3455d71c2469a50d5e2aae65bc81
David B <barberi.office@gmail.com>
2016-05-29 13:09:41 +0200

    versions, icons, toc

--- admin/info.php
+++ admin/info.php
@@ -1,16 +1,20 @@
 <?php
-// Attogram Framework - Site Info v0.0.3
+// Attogram Framework - Site Info v0.0.4
 
 namespace Attogram;
 
 global $debug;
-
-$this->page_header('Site Info');
+$title = 'Information';
+$this->page_header($title);
 
 $info = array();
 
-$info['<h3><span class="glyphicon glyphicon-info-sign"></span> <em>Attogram:</em></h3>'] = '';
-$info['ATTOGRAM_VERSION'] = ATTOGRAM_VERSION;
+$info['<a name="attogram"></a><h3><span class="glyphicon glyphicon-flash"></span> <em>Attogram:</em></h3>'] = '';
+$info['Attogram Version'] = ATTOGRAM_VERSION;
+$info['Apache Version'] = apache_get_version();
+$info['PHP Version'] = phpversion();
+
+$info['<a name="site"></a><h3><span class="glyphicon glyphicon-home"></span> <em>Site:</em></h3>'] = '';
 $info['site_name'] = $this->site_name;
 $info['site_url'] = '<a href="' . $this->get_site_url() . '">' . $this->get_site_url() . '</a>';
 $info['path'] = ( $this->path ? $this->path : '<code>Top Level</code>' );
@@ -22,11 +26,11 @@ $info['depth'] =  info_array($this->depth,$keyed=1);
 $info['force_slash_exceptions'] =  info_array($this->force_slash_exceptions);
 $info['admins'] = info_array($this->admins);
 
-$info['<h3><span class="glyphicon glyphicon-play"></span> <em>Actions:</em></h3>'] = '';
+$info['<a name="actions"></a><h3><span class="glyphicon glyphicon-play"></span> <em>Actions:</em></h3>'] = '';
 $info['actions'] = info_actions($this->actions);
 $info['admin_actions'] = info_actions($this->admin_actions);
 
-$info['<h3><span class="glyphicon glyphicon-folder-open"></span> <em>Directories:</em></h3>'] = '';
+$info['<a name="directories"></a><h3><span class="glyphicon glyphicon-folder-open"></span> <em>Directories:</em></h3>'] = '';
 $info['attogram_directory'] = info_dir($this->attogram_directory);
 $info['actions_dir'] = info_dir($this->actions_dir);
 $info['admin_dir'] = info_dir($this->admin_dir);
@@ -36,7 +40,7 @@ $info['tables_dir'] = info_dir($this->tables_dir);
 $info['functions_dir'] = info_dir($this->functions_dir);
 $info['skip_files'] = info_array( $this->skip_files);
 
-$info['<h3><span class="glyphicon glyphicon-file"></span> <em>Files:</em></h3>'] = '';
+$info['<a name="files"></a><h3><span class="glyphicon glyphicon-file"></span> <em>Files:</em></h3>'] = '';
 $info['action'] = info_file($this->action);
 $info['autoloader'] = info_file($this->autoloader);
 $info['fof'] = info_file($this->fof);
@@ -44,23 +48,33 @@ $info['header'] = info_file($this->templates_dir . '/header.php');
 $info['nav_bar'] = info_file($this->templates_dir . '/navbar.php');
 $info['footer'] = info_file($this->templates_dir . '/footer.php');
 
-$info['<h3><span class="glyphicon glyphicon-paperclip"></span> <em>Objects:</em></h3>'] = '';
+$info['<a name="objects"></a><h3><span class="glyphicon glyphicon-paperclip"></span> <em>Objects:</em></h3>'] = '';
 $info['request'] = info_object($this->request);
 $info['session'] = info_object($this->session);
 $info['log'] = info_object($this->log);
 
-$info['<h3><span class="glyphicon glyphicon-hdd"></span> <em>Database:</em></h3>'] = '';
+$info['<a name="database"></a><h3><span class="glyphicon glyphicon-hdd"></span> <em>Database:</em></h3>'] = '';
 $info['sqlite_database'] = info_object($this->sqlite_database);
 $info['db_name'] = info_file($this->db_name);
 $info['database_size'] = (file_exists($this->db_name) ? filesize($this->db_name) : '<code>null</code>') . ' bytes';
 
-$info['<h3><span class="glyphicon glyphicon-user"></span> <em>User:</em></h3>'] = '';
+$info['<a name="user"></a><h3><span class="glyphicon glyphicon-user"></span> <em>User:</em></h3>'] = '';
 $info['# session attributes'] = $this->session->count();
 $info['session attributes'] = info_array( $this->session->all(), $key=1 );
 
 print '
 <div class="container">
- <h1>Site Info</h1>
+ <h1><span class="glyphicon glyphicon-info-sign"></span> ' . $title . '</h1>
+ <p>
+ <a href="#attogram">attogram</a> -
+ <a href="#site">site</a> -
+ <a href="#actions">actions</a> -
+ <a href="#directories">directories</a> -
+ <a href="#files">files</a> -
+ <a href="#objects">objects</a> -
+ <a href="#database">database</a> -
+ <a href="#user">user</a>
+ </p>
  <table class="table table-condensed">
 ';
 
