e6a06cb6cbda794c0d61f6f318fa4897dce63fca
David B <barberi.office@gmail.com>
2016-06-01 20:26:42 +0200

    if not exists

--- modules/list/tables/list.sql
+++ modules/list/tables/list.sql
@@ -1,5 +1,7 @@
-CREATE TABLE 'list' (
-'id' INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+-- Attogram Framework - List Module - list table - v0.0.1
+
+CREATE TABLE IF NOT EXISTS 'list' (
+'id' INTEGER PRIMARY KEY,
 'list' TEXT NOT NULL,
 'item' TEXT NOT NULL,
 'ordering' INTEGER NOT NULL DEFAULT 0
