548ec50aeef8cc7a11513a1043293c81a13fbf03
David B <barberi.office@gmail.com>
2016-06-07 12:41:31 +0200

    Support-ish IE + Bootstrap

--- templates/header.php
+++ templates/header.php
@@ -8,9 +8,15 @@ if( !isset($title) || !$title || !is_string($title) ) {
 
 ?><!DOCTYPE html>
 <html lang="en"><head><meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="<?php print $this->path; ?>/web/bootstrap/css/bootstrap.min.css">
 <link rel="stylesheet" href="<?php print $this->path; ?>/web/attogram.css">
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+ <!--[if lt IE 9]>
+   <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+   <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+ <![endif]-->
 <title><?php print $title; ?></title>
 <script src="<?php print $this->path; ?>/web/jquery.min.js"></script>
 <script src="<?php print $this->path; ?>/web/bootstrap/js/bootstrap.min.js"></script>
