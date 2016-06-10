639de50a5ff47d87adbb8e54d073ca2899712dbe
David B <barberi.office@gmail.com>
2016-06-10 20:39:11 +0200

    🚀 Powered by

--- templates/footer.php
+++ templates/footer.php
@@ -1,18 +1,18 @@
-<?php // Attogram Framework - Page Footer v0.0.5
+<?php // Attogram Framework - Page Footer v0.0.6
 
 namespace Attogram;
 
 ?>
 <footer class="footer">
  <div class="container-fluid">
-  <p><small>
-    <strong>🔧</strong> Powered by
-    <a target="github" href="<?php print $this->project_github; ?>">Attogram Framework v<?php print ATTOGRAM_VERSION; ?></a>
-    &nbsp;&nbsp; - &nbsp;&nbsp;
-    📆 <?php print gmdate('Y-m-d H:i:s'); ?> UTC
-    &nbsp;&nbsp; - &nbsp;&nbsp;
-    Page made with 💙 in <?php print round( (microtime(1) - $this->start_time), 4); ?> seconds
-  </small></p>
+  <p>
+    <nobr>🚀 Powered by the
+    <a target="github" href="<?php print $this->project_github; ?>">Attogram Framework v<?php print ATTOGRAM_VERSION; ?></a></nobr>
+    &nbsp;&nbsp; <small>|</small> &nbsp;&nbsp;
+    <nobr>📆 <?php print gmdate('Y-m-d H:i:s'); ?> UTC</nobr>
+    &nbsp;&nbsp; <small>|</small> &nbsp;&nbsp;
+    <nobr>💙 Page generated in <?php print round( (microtime(1) - $this->start_time), 4); ?> seconds</nobr>
+  </p>
  </div>
 </footer>
 <?php
