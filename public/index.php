2017cf83c75e6f0e133c112329c4ae7bab2adcc8
David B <barberi.office@gmail.com>
2016-06-14 00:12:49 +0200

    rm load.php, move into loader tranquility()

--- public/index.php
+++ public/index.php
@@ -150,18 +150,47 @@ class guru_meditation_loader
   } // end function inner_awareness()
 
   function tranquility() {
-      $this->debug('tranquility: ' . $this->project_loader);
-      if( !is_file($this->project_loader) ) {
-        $this->guru_meditation_error('Project loader file missing: ' . $this->project_loader);
+      global $config;
+      $this->debug('tranquility');
+
+      if( !ob_start("ob_gzhandler") ) { // speed things up! gzip buffer
+        ob_start(); // if gzip handler not available, do normal buffer
       }
-      if( !is_readable($this->project_loader) ) {
-        $this->guru_meditation_error('Project loader file exists, but is not readable: ' . $this->project_loader);
+
+      // Setup Monolog
+      if(
+          ( isset($config['debug']) && is_bool($config['debug']) && $config['debug'] ) // $config['debug'] = true
+        ||
+          ( isset($_GET['debug'])   // admin debug url override ?debug
+            && isset($config['admins'])
+            && is_array($config['admins'])
+            && in_array($_SERVER['REMOTE_ADDR'], $config['admins'])
+          )
+      ) {
+        $log = new \Monolog\Logger('attogram');
+        $sh = new \Monolog\Handler\StreamHandler('php://output');
+        $format = "<p class=\"text-danger squished\">%datetime%|%level_name%: %message% %context%</p>"; // %extra%
+        $dateformat = 'Y-m-d|H:i:s:u';
+        $sh->setFormatter( new \Monolog\Formatter\LineFormatter($format, $dateformat) );
+        $log->pushHandler( new \Monolog\Handler\BufferHandler($sh) );
+        // $log->pushHandler( new \Monolog\Handler\BrowserConsoleHandler ); // dev
+      } else {
+        $log = new \Attogram\logger();
       }
-      $project_loader_included = ( include($this->project_loader) );
-      if( !$project_loader_included ) {
-        $this->guru_meditation_error('Project loader file exists, but include failed: ' . $this->project_loader);
+
+      if( isset($config['guru_meditation_loader']) && is_array($config['guru_meditation_loader']) ) {
+        foreach( $config['guru_meditation_loader'] as $g ) {
+          $log->debug($g); // save loader debug log
+        }
       }
-  }
+
+      if( !isset($config['debug']) ) {
+        $config['debug'] = false;
+      }
+
+      $attogram = new attogram( $log, $config['debug'] ); // Start Attogram Framework!
+
+  } // end function tranquility()
 
   function debug( $msg ) {
     global $config;
