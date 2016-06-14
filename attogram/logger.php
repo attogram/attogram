c05a7583413d72f3cd4968582b39c7b15bfca8c9
David B <barberi.office@gmail.com>
2016-06-14 09:35:59 +0200

    support php5 type hinting only

--- attogram/logger.php
+++ attogram/logger.php
@@ -1,4 +1,4 @@
-<?php // Attogram Framework - logger v0.0.1
+<?php // Attogram Framework - logger v0.0.2
 
 namespace Attogram;
 
@@ -8,15 +8,15 @@ namespace Attogram;
 class logger
 {
   public $stack;
-  public function log( string $level, string $message, array $context = array() ) {
+  public function log( $level, $message, array $context = array() ) {
     $this->stack[] = "$level: $message" . ( $context ? ': ' . print_r($context,1) : '');
   }
-  public function emergency( string $message, array $context = array()) { $this->log('emergency',$message,$context); }
-  public function alert(     string $message, array $context = array()) { $this->log('alert',    $message,$context); }
-  public function critical(  string $message, array $context = array()) { $this->log('critical', $message,$context); }
-  public function error(     string $message, array $context = array()) { $this->log('error',    $message,$context); }
-  public function warning(   string $message, array $context = array()) { $this->log('warning',  $message,$context); }
-  public function notice(    string $message, array $context = array()) { $this->log('notice',   $message,$context); }
-  public function info(      string $message, array $context = array()) { $this->log('info',     $message,$context); }
-  public function debug(     string $message, array $context = array()) { $this->log('debug',    $message,$context); }
+  public function emergency( $message, array $context = array()) { $this->log('emergency',$message,$context); }
+  public function alert(     $message, array $context = array()) { $this->log('alert',    $message,$context); }
+  public function critical(  $message, array $context = array()) { $this->log('critical', $message,$context); }
+  public function error(     $message, array $context = array()) { $this->log('error',    $message,$context); }
+  public function warning(   $message, array $context = array()) { $this->log('warning',  $message,$context); }
+  public function notice(    $message, array $context = array()) { $this->log('notice',   $message,$context); }
+  public function info(      $message, array $context = array()) { $this->log('info',     $message,$context); }
+  public function debug(     $message, array $context = array()) { $this->log('debug',    $message,$context); }
 } // end class logger
