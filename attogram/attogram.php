e86a07f307380036d919dfc54c5a6d55c76399dc
David B <barberi.office@gmail.com>
2016-06-16 01:27:36 +0200

    v0.6.1

--- attogram/attogram.php
+++ attogram/attogram.php
@@ -12,7 +12,7 @@ namespace Attogram;
  * The Attogram Framework is Dual Licensed under the MIT License (MIT)
  * _or_ the GNU General Public License version 3 or higher (GPL-3.0+).
  *
- * @version 0.6.1-dev
+ * @version 0.6.1
  * @license MIT
  * @license GPL-3.0+
  * @copyright 2016 Attogram Framework Developers https://github.com/attogram/attogram
@@ -20,7 +20,7 @@ namespace Attogram;
 class attogram
 {
 
-  const ATTOGRAM_VERSION = '0.6.1-dev';
+  const ATTOGRAM_VERSION = '0.6.1';
 
   public $start_time;    // (float) microsecond time of awakening
   public $debug;         // (boolean) debug on/off
