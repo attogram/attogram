514aeec63ae69fb8542f83393460e4b6ccb17cf9
David B <barberi.office@gmail.com>
2016-06-19 01:05:26 +0200

    interface attogram_database to own file

--- modules/database/includes/sqlite_database.php
+++ modules/database/includes/sqlite_database.php
@@ -1,37 +1,6 @@
-<?php  // Attogram Framework - Database Module - sqlite_database class v0.1.0
+<?php  // Attogram Framework - Database Module - sqlite_database class v0.1.1
 
 namespace Attogram;
-
-/**
- * Attogram Database Object Interface
- */
-interface attogram_database
-{
-
-  /**
-   * Query the database, return an array of results
-   * @param  string $sql  The SQL query
-   * @param  array  $bind (optional) Array of name/values to bind into the SQL query
-   * @return array        An array of results
-   */
-  public function query( $sql, $bind = array() );
-
-  /**
-   * Query the database, return only true or false
-   * @param  string $sql  The SQL query
-   * @param  array  $bind (optional) Array of name/values to bind into the SQL query
-   * @return bool         true on successful query, false on error
-   */
-  public function queryb( $sql, $bind = array() );
-
-  /**
-   * Initialize the database connection
-   * @return bool true on successful initialization, false on error
-   */
-  public function init_db();
-
-} // end interface attogram_database
-
 
 /**
  * Attogram sqlite_database
