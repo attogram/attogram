3b97137977869e9c53b2fe2b281007005ed193cc
David B <barberi.office@gmail.com>
2016-07-01 16:52:28 +0200

    v0.6.8

--- attogram/attogram.php
+++ attogram/attogram.php
@@ -20,7 +20,7 @@ namespace Attogram;
 class attogram
 {
 
-  const ATTOGRAM_VERSION = '0.6.8-dev';
+  const ATTOGRAM_VERSION = '0.6.8';
 
   public $start_time;    // (float) microsecond time of awakening
   public $debug;         // (boolean) debug on/off
