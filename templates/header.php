4fc7f3229615c79a764dbe31ccb70a8fdf8b93be
David B <barberi.office@gmail.com>
2016-07-01 12:52:39 +0200

    default title as site_name, spacing

--- templates/header.php
+++ templates/header.php
@@ -1,9 +1,9 @@
-<?php // Attogram Framework - Page Header v0.0.4
+<?php // Attogram Framework - Page Header v0.0.5
 
 namespace Attogram;
 
 if( !isset($title) || !$title || !is_string($title) ) {
-  $title = 'Attogram Framework';
+  $title = $this->site_name;
 }
 
 ?><!DOCTYPE html>
@@ -12,18 +12,20 @@ if( !isset($title) || !$title || !is_string($title) ) {
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="<?php print $this->path; ?>/web/bootstrap/css/bootstrap.min.css">
 <link rel="stylesheet" href="<?php print $this->path; ?>/web/attogram.css">
- <!--[if lt IE 9]>
-   <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-   <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
- <![endif]-->
+<!--[if lt IE 9]>
+<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
 <title><?php print $title; ?></title>
 <script src="<?php print $this->path; ?>/web/jquery.min.js"></script>
 <script src="<?php print $this->path; ?>/web/bootstrap/js/bootstrap.min.js"></script>
 </head><body><noscript><div class="alert alert-danger">Please enable Javascript</div></noscript>
 <?php
+
 $navbar = $this->templates['navbar'];
+
 if( attogram_fs::is_readable_file( $navbar, 'php' ) ) {
-  include( $navbar );
+  include($navbar);
 } else {
-  $this->log->error("Missing navbar template: $navbar");
+  $this->log->error('Missing navbar template: ' . $navbar);
 }
