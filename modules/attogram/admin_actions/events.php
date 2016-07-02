069893251c377eb35928a54cc86ee218d794f2f9
David B <barberi.office@gmail.com>
2016-07-03 00:03:55 +0200

    prettify

--- modules/attogram/admin_actions/events.php
+++ modules/attogram/admin_actions/events.php
@@ -1,15 +1,31 @@
-<?php // Attogram Framework - Events log v0.0.2
+<?php // Attogram Framework - Events log v0.0.3
 
 namespace Attogram;
 
 $this->page_header('Event Log');
 print '<div class="container"><h1>Event Log</h1>';
-print '<p>last 500 events:</p>';
+print '<p>last 1000 events:</p>';
 
-$e = $this->db->query('SELECT * FROM event ORDER BY time DESC LIMIT 500');
+$e = $this->db->query('SELECT * FROM event ORDER BY id DESC LIMIT 1000');
 
 foreach( $e as $v ) {
-  print $this->web_display( $v['message'] ) . '<br />';
+  $vm = explode( ' ', $v['message'] );
+  $datetime = ltrim( $vm[0], '[' ) . ' ' . rtrim( $vm[1], ']');
+  $type = rtrim( $vm[2], ':' );
+  $type = preg_replace('/^event\./', '', $type);
+  $trash = array_shift($vm); $trash = array_shift($vm); $trash = array_shift($vm);
+  $message = implode(' ', $vm);
+  $message = rtrim($message); $message = rtrim($message, '[..]'); $message = rtrim($message);
+  $message = rtrim($message); $message = rtrim($message, '[..]'); $message = rtrim($message);
+
+  print '
+  <div class="row" style="border:1px solid #ccc;">
+   <div class="col-sm-2"><small>' . $datetime . '</small></div>
+   <div class="col-sm-1"><small>' . $type . '</small></div>
+   <div class="col-sm-9">' . $this->web_display($message) . '</div>
+  </div>
+  ';
+
 }
 
 print '</div>';
