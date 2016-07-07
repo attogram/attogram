d144767c93b3fc5789faef351be5412be352ca3e
Attogram <barberi.office@gmail.com>
2016-07-07 14:14:24 +0200

    User module now lives at: https://github.com/attogram/attogram-user

--- modules/_user/tables/user.sql
+++
@@ -1,9 +0,0 @@
--- Attogram Framework - User Module - user table - v0.1.1
-
-CREATE TABLE IF NOT EXISTS 'user' (
-'id' INTEGER PRIMARY KEY,
-'username' TEXT UNIQUE NOT NULL,
-'password' TEXT NOT NULL,
-'email' TEXT NOT NULL,
-'level' INTEGER NOT NULL DEFAULT '0'
-)
