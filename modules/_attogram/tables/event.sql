0f2d9ebe2f427ea154c267edc1e3d789e7c59c88
Attogram <barberi.office@gmail.com>
2016-07-07 14:15:00 +0200

    Base module now lives at: https://github.com/attogram/attogram-base

--- modules/_attogram/tables/event.sql
+++
@@ -1,9 +0,0 @@
--- Attogram Framework - event table - v0.0.1
-
-CREATE TABLE IF NOT EXISTS 'event' (
-'id' INTEGER PRIMARY KEY,
-'time' INTEGER,
-'channel' TEXT,
-'level' INTEGER,
-'message' TEXT
-)
