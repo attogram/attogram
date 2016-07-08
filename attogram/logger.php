24c700c64e2919fe8f0389055a5b77576193126a
Attogram <barberi.office@gmail.com>
2016-07-08 22:55:53 +0200

    Null PSR-3 Logger

--- attogram/logger.php
+++ attogram/logger.php
@@ -1,58 +1,47 @@
-<?php // Attogram Framework - logger class v0.0.5
+<?php // Attogram Framework - logger class v0.1.0
 
 namespace Attogram;
 
 /**
- * Null-ish Stack-ish PSR3 Logger
+ * Null PSR-3 Logger
  */
 class logger implements \Psr\Log\LoggerInterface
 {
 
-  public $stack;
-
   public function log( $level, $message, array $context = array() )
   {
-    $this->stack[] = "$level: $message" . ( $context ? ': ' . print_r( $context, 1 ) : '');
   }
 
   public function emergency( $message, array $context = array() )
   {
-    $this->log( 'emergency', $message, $context );
   }
 
   public function alert( $message, array $context = array() )
   {
-    $this->log( 'alert', $message, $context );
   }
 
   public function critical( $message, array $context = array() )
   {
-    $this->log( 'critical', $message, $context );
   }
 
   public function error( $message, array $context = array() )
   {
-    $this->log('error', $message, $context );
   }
 
   public function warning( $message, array $context = array() )
   {
-    $this->log( 'warning', $message, $context );
   }
 
   public function notice( $message, array $context = array() )
   {
-    $this->log( 'notice', $message ,$context );
   }
 
   public function info( $message, array $context = array() )
   {
-    $this->log( 'info', $message, $context );
   }
 
   public function debug( $message, array $context = array() )
   {
-    $this->log( 'debug', $message, $context );
   }
 
 } // end class logger
