067478bf77b3f443f95fb25f471c064babe214ec
Attogram <barberi.office@gmail.com>
2016-07-11 12:04:25 +0200

    psr1/psr2

--- attogram/attogram_fs.php
+++ attogram/attogram_fs.php
@@ -1,5 +1,5 @@
 <?php
-// Attogram Framework - attogram_fs class v0.1.0
+// Attogram Framework - attogram_fs class v0.1.1
 
 namespace attogram;
 
@@ -9,154 +9,150 @@ namespace attogram;
 class attogram_fs
 {
     /**
-   * Get list of all sub-subdirectories of a specific name:  $dir/[*]/$name.
-   *
-   * @param string $dir  The directory to search within (ie: modules directory)
-   * @param string $name The name of the subdirectories to find
-   *
-   * @return array       List of the directories found
-   */
-  public static function get_all_subdirectories($dir, $name)
-  {
-      if (!isset($dir) || !$dir || !is_string($dir) || !is_readable($dir)) {
-          return array();
-      }
-      $result = array();
-      foreach (array_diff(scandir($dir), self::get_skip_files()) as $d) {
-          $md = $dir.'/'.$d;
-          if (!is_readable($md)) {
-              continue;
-          }
-          $md .= '/'.$name;
-          if (!is_readable($md)) {
-              continue;
-          }
-          $result[] = $md;
-      }
+     * Get list of all sub-subdirectories of a specific name:  $dir/[*]/$name.
+     *
+     * @param string $dir  The directory to search within (ie: modules directory)
+     * @param string $name The name of the subdirectories to find
+     *
+     * @return array       List of the directories found
+     */
+    public static function get_all_subdirectories($dir, $name)
+    {
+        if (!isset($dir) || !$dir || !is_string($dir) || !is_readable($dir)) {
+            return array();
+        }
+        $result = array();
+        foreach (array_diff(scandir($dir), self::get_skip_files()) as $d) {
+            $md = $dir.'/'.$d;
+            if (!is_readable($md)) {
+                continue;
+            }
+            $md .= '/'.$name;
+            if (!is_readable($md)) {
+                continue;
+            }
+            $result[] = $md;
+        }
+        return $result;
+    } // end function get_all_subdirectories()
 
-      return $result;
-  } // end function get_all_subdirectories()
+    /**
+     * Include all php files in a specific directory.
+     *
+     * @param  string $dir The directory to search
+     *
+     * @return array       List of the files successfully included
+     */
+    public static function include_all_php_files_in_directory($dir)
+    {
+        $included = array();
+        if (!is_readable($dir)) {
+            return $included;
+        }
+        foreach (array_diff(scandir($dir), self::get_skip_files()) as $f) {
+            $ff = $dir.'/'.$f;
+            if (!self::is_readable_file($ff, '.php')) {
+                continue;
+            }
+            if ((include($ff))) {
+                $included[] = $ff;
+            }
+        }
+        return $included;
+    } // end function include_all_php_files_in_directory()
 
-  /**
-   * Include all php files in a specific directory.
-   *
-   * @param  string $dir The directory to search
-   *
-   * @return array       List of the files successfully included
-   */
-  public static function include_all_php_files_in_directory($dir)
-  {
-      $included = array();
-      if (!is_readable($dir)) {
-          return $included;
-      }
-      foreach (array_diff(scandir($dir), self::get_skip_files()) as $f) {
-          $ff = $dir.'/'.$f;
-          if (!self::is_readable_file($ff, '.php')) {
-              continue;
-          }
-          if ((include($ff))) {
-              $included[] = $ff;
-          }
-      }
+    /**
+     * Tests if is a file exist, is readable, and is of a certain type.
+     *
+     * @param  string $file The name of the file to test
+     * @param  string $type (optional) The file extension to allow. Defaults to '.php'
+     *
+     * @return bool
+     */
+    public static function is_readable_file($file = false, $type = '.php')
+    {
+        if (!$file || !$type || $type == '' || !is_string($type) || !is_string($file) || !is_readable($file)) {
+            return false;
+        }
+        if (preg_match('/'.$type.'$/', $file)) {
+            return true;
+        }
 
-      return $included;
-  } // end function include_all_php_files_in_directory()
-
-  /**
-   * Tests if is a file exist, is readable, and is of a certain type.
-   *
-   * @param  string $file The name of the file to test
-   * @param  string $type (optional) The file extension to allow. Defaults to '.php'
-   *
-   * @return bool
-   */
-  public static function is_readable_file($file = false, $type = '.php')
-  {
-      if (!$file || !$type || $type == '' || !is_string($type) || !is_string($file) || !is_readable($file)) {
-          return false;
-      }
-      if (preg_match('/'.$type.'$/', $file)) {
-          return true;
-      }
-
-      return false;
-  }
-
-  /**
-   * get an array of filenames to skip.
-   *
-   * @return array
-   */
-  public static function get_skip_files()
-  {
-      return array('.', '..', '.htaccess');
-  }
-
-  /**
-   * Examines each module for a named subdirectory, then includes all *.php files from that directory.
-   *
-   * @param string $modules_directory
-   *
-   * @return array List of the files successfully loaded
-   */
-  public static function load_module_subdirectories($modules_directory, $subdirectory)
-  {
-      global $config;
-      $included = array();
-      $dirs = self::get_all_subdirectories($modules_directory, $subdirectory);
-      if (!$dirs) {
-          return $included;
-      }
-      foreach ($dirs as $d) {
-          $inc = self::include_all_php_files_in_directory($d);
-          $included = array_merge($included, $inc);
-      }
-
-      return $included;
-  } // end function load_module_subdirectories()
-
-  /**
-   * get the mime type of a file.
-   *
-   * @param string $file  The file to examine
-   *
-   * @return string       The mime type, or false
-   */
-  public static function get_mime_type($file)
-  {
-      $mime_type = finfo_file(finfo_open(FILEINFO_MIME_TYPE), $file);
-      switch (pathinfo($file, PATHINFO_EXTENSION)) { // https://bugs.php.net/bug.php?id=53035
-      case 'css':
-        $mime_type = 'text/css';
-        break;
-      case 'js':
-        $mime_type = 'application/javascript';
-        break;
-      case 'xml':
-        $mime_type = 'text/xml';
-        break;
-      case 'php':
-        $mime_type = false; // do not do content type header, not needed for native php
-        break;
-      case 'eot':
-        $mime_type = 'application/vnd.ms-fontobject';
-        break;
-      case 'svg':
-        $mime_type = 'image/svg+xml';
-        break;
-      case 'ttf':
-        $mime_type = 'application/font-sfnt';
-        break;
-      case 'woff':
-        $mime_type = 'application/font-woff';
-        break;
-      case 'woff2':
-        $mime_type = 'application/font-woff2';
-        break;
-
+        return false;
     }
 
-      return $mime_type;
-  }
+    /**
+     * get an array of filenames to skip.
+     *
+     * @return array
+     */
+    public static function get_skip_files()
+    {
+        return array('.', '..', '.htaccess');
+    }
+
+    /**
+     * Examines each module for a named subdirectory, then includes all *.php files from that directory.
+     *
+     * @param string $modules_directory
+     *
+     * @return array List of the files successfully loaded
+     */
+    public static function load_module_subdirectories($modules_directory, $subdirectory)
+    {
+        global $config;
+        $included = array();
+        $dirs = self::get_all_subdirectories($modules_directory, $subdirectory);
+        if (!$dirs) {
+            return $included;
+        }
+        foreach ($dirs as $d) {
+            $inc = self::include_all_php_files_in_directory($d);
+            $included = array_merge($included, $inc);
+        }
+        return $included;
+    } // end function load_module_subdirectories()
+
+    /**
+     * get the mime type of a file.
+     *
+     * @param string $file  The file to examine
+     *
+     * @return string       The mime type, or false
+     */
+    public static function get_mime_type($file)
+    {
+        $mime_type = finfo_file(finfo_open(FILEINFO_MIME_TYPE), $file);
+        switch (pathinfo($file, PATHINFO_EXTENSION)) { // https://bugs.php.net/bug.php?id=53035
+            case 'css':
+                $mime_type = 'text/css';
+                break;
+            case 'js':
+                $mime_type = 'application/javascript';
+                break;
+            case 'xml':
+                $mime_type = 'text/xml';
+                break;
+            case 'php':
+                $mime_type = false; // do not do content type header, not needed for native php
+                break;
+            case 'eot':
+                $mime_type = 'application/vnd.ms-fontobject';
+                break;
+            case 'svg':
+                $mime_type = 'image/svg+xml';
+                break;
+            case 'ttf':
+                $mime_type = 'application/font-sfnt';
+                break;
+            case 'woff':
+                $mime_type = 'application/font-woff';
+                break;
+            case 'woff2':
+                $mime_type = 'application/font-woff2';
+                break;
+        }
+        return $mime_type;
+    }
+
 } // end class attogram_fs
