d75dbc6eb8b8537516561ba5bf9b7a7af74e2f21
Attogram <barberi.office@gmail.com>
2016-07-15 19:16:42 +0200

    compress html

--- templates/footer.php
+++ templates/footer.php
@@ -1,25 +1,20 @@
 <?php
-// Attogram Framework - Page Footer v0.2.3
+// Attogram Framework - Page Footer v0.3.0
 
 namespace Attogram;
 
 $divider = '&nbsp;&nbsp; | &nbsp;&nbsp;';
-echo '
-<footer class="footer">
- <div class="container-fluid">
-  <p>
-    <small>
-    <span style="white-space: nowrap"><a href="'.$this->getSiteUrl().'/">'.$this->siteName.'</a></span>
-    '.$divider.'
-    <span style="white-space: nowrap">🚀 Powered by <a target="github" href="'.$this->projectRepository.'">Attogram <small>v'.attogram::ATTOGRAM_VERSION.'</small></a></span>
-    '.$divider.'
-    <span style="white-space: nowrap">🕑 '.gmdate('Y-m-d H:i:s').' UTC</span>
-    '.$divider.'
-    <span style="white-space: nowrap">👤 '.$this->request->getClientIp().'</span>
-    '.$divider.'
-    <span style="white-space: nowrap">🏁 '.round((microtime(1) - $this->startTime), 6, PHP_ROUND_HALF_UP).' seconds</span>
-    </small>
-  </p>
- </div>
-</footer>
-</body></html>';
+echo
+    '<footer class="footer">',
+    '<div class="container-fluid">',
+    '<p><small>',
+    '<span style="white-space: nowrap"><a href="'.$this->getSiteUrl().'/">'.$this->siteName.'</a></span>',
+    $divider,
+    '<span style="white-space: nowrap">🚀 Powered by <a target="github" href="'.$this->projectRepository.'">Attogram <small>v'.attogram::ATTOGRAM_VERSION.'</small></a></span>',
+    $divider,
+    '<span style="white-space: nowrap">🕑 '.gmdate('Y-m-d H:i:s').' UTC</span>',
+    $divider,
+    '<span style="white-space: nowrap">👤 '.$this->request->getClientIp().'</span>',
+    $divider,
+    '<span style="white-space: nowrap">🏁 '.round((microtime(1) - $this->startTime), 6, PHP_ROUND_HALF_UP).' seconds</span>',
+    '</small></p></div></footer></body></html>';
--- templates/navbar.php
+++ templates/navbar.php
@@ -1,58 +1,53 @@
-<?php // Attogram Framework - Navbar v0.1.6
+<?php // Attogram Framework - Navbar v0.2.0
 
 namespace Attogram;
 
-?>
-<nav class="navbar navbar-default">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" href="<?php echo $this->path; ?>/"><?php echo $this->siteName; ?></a>
-    </div>
-    <div id="navbar" class="navbar-collapse collapse">
-      <ul class="nav navbar-nav"><?php
-        foreach (array_keys($this->getActions()) as $a) {
-            switch ($a) {
-              case 'home': case 'login': case 'user':
-                continue;
-              default:
-                print '<li><a href="'.$this->path.'/'.$a.'/">'.$a.'</a></li>';
-                break;
-            }
-        }
-    ?></ul>
-      <ul class="nav navbar-nav navbar-right">
-<?php
-    if (class_exists('\attogram\AttogramUser')) {
-        if (\attogram\AttogramUser::isLoggedIn()) {
-            echo '<li><a href="'.$this->path.'/user/"><span class="icon-s">👤</span> <b>'
+echo '<nav class="navbar navbar-default">'
+.'<div class="container-fluid">'
+.'<div class="navbar-header">'
+.'<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" '
+.'data-target="#navbar" aria-expanded="false" aria-controls="navbar">'
+.'<span class="sr-only">Toggle navigation</span><span class="icon-bar"></span>'
+.'<span class="icon-bar"></span><span class="icon-bar"></span></button>'
+.'<a class="navbar-brand" href="'.$this->path.'/">'.$this->siteName.'</a></div>'
+.'<div id="navbar" class="navbar-collapse collapse"><ul class="nav navbar-nav">';
+
+foreach (array_keys($this->getActions()) as $a) {
+    switch ($a) {
+        case 'home':
+        case 'login':
+        case 'user':
+            continue;
+        default:
+            print '<li><a href="'.$this->path.'/'.$a.'/">'.$a.'</a></li>';
+            break;
+    }
+}
+
+print '</ul><ul class="nav navbar-nav navbar-right">';
+
+if (class_exists('\attogram\AttogramUser')) {
+    if (\attogram\AttogramUser::isLoggedIn()) {
+        echo '<li><a href="'.$this->path.'/user/"><span class="icon-s">👤</span> <b>'
             .((isset($_SESSION['AttogramUsername']) && $_SESSION['AttogramUsername'])  ? $_SESSION['AttogramUsername'] : 'user')
-            .'</b></a></li>';
-            echo '<li><a href="?logoff"><span class="glyphicon glyphicon-remove-circle" aria-hidden="true"></span> logoff</a></li>';
-        } else {
-
-            if (array_key_exists('login', $this->getActions())) { // if User Module is loaded
-                echo '<li><a href="'.$this->path
+            .'</b></a></li>'
+            .'<li><a href="?logoff"><span class="glyphicon glyphicon-remove-circle" aria-hidden="true"></span> logoff</a></li>';
+    } else {
+        if (array_key_exists('login', $this->getActions())) { // if User Module is loaded
+            echo '<li><a href="'.$this->path
                 .'/login/">login <span class="glyphicon glyphicon-log-in" aria-hidden="true"></span></a></li>';
-            }
         }
-    }  // end if user module active
+    }
+}  // end if user module active
 
-    if ($this->isAdmin() && $this->getAdminActions()) {
-        echo '<li class="dropdown">'
+if ($this->isAdmin() && $this->getAdminActions()) {
+    echo '<li class="dropdown">'
         .'<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">'
         .'Admin <span class="caret"></span></a><ul class="dropdown-menu">';
-        foreach (array_keys($this->getAdminActions()) as $a) {
-            echo '<li><a href="'.$this->path.'/'.$a.'/">'.$a.'</a></li>';
-        }
-        echo '</ul></li>';
+    foreach (array_keys($this->getAdminActions()) as $a) {
+        echo '<li><a href="'.$this->path.'/'.$a.'/">'.$a.'</a></li>';
     }
-    ?></ul>
-    </div><!--/.nav-collapse -->
-  </div><!--/.container-fluid -->
-</nav>
+    echo '</ul></li>';
+}
+
+echo '</ul></div></div></nav>';
